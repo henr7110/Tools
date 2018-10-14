4f37ab1a23f1974d6489b483f18795e59696af3b
Henrik Pinholt <henrikpinholt@hotmail.com>
2018-10-14 17:44:00 +0200

    Made sections a little more visible

--- xyz translator.py
+++ xyz translator.py
@@ -26,7 +26,7 @@ def coordshifter(mtimes,line):
     newline = "%s        %.18g        %.18g        %.18g" %(vals[0],float(vals[1]),float(vals[2]),float(vals[3]))
     newfile.append(newline)
 
-#Initialize
+#-------------Initialize-----------
 n = 1 #Number of translations
 path = "Cluster_start_NaCl_2M.xyz"
 file = open(path,"r")
@@ -36,13 +36,13 @@ i = 0
 shiftvals = []
 filename = "NaCl-trans1small.xyz" # name of output file
 
-#Generate the translation vectors
+#------------Generate the translation vectors--------
 for x in range(n+1):
     for y in range(n+1):
         for z in range(n+1):
             shiftvals.append([x,y,z])
 
-#Itterate through the xyz file and translate all atoms
+#-----------Itterate through the xyz file and translate all atoms---------
 for line in file:
     #Copy header to new file
     if i ==1 or i == 0:
@@ -52,7 +52,7 @@ for line in file:
             coordshifter(coords,line)
     i += 1
 
-# Save the new xyz file
+# ----------Save the new xyz file---------
 out = open(filename,"w")
 out.write(str(len(newfile[2:])) + "\n")
 for i in newfile[1:]:
